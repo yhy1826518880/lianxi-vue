daf1bd4511f68359072e9616bbf6a4e98334dbd5
yhy1826518880 <1826518880@qq.com>
2019-03-27 19:39:46 +0800

    导航布局头部退出功能完成

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,58 +1,171 @@
 <template>
-  <!-- <div>home------</div> -->
   <el-container class="container">
-    <el-aside class="aside" width="200px">
-     
-    </el-aside>
-    <el-container>
-      <el-header class="header">
-           <el-row>
+    <el-header class="header">
+      <el-row>
         <el-col :span="4">
           <div class="grid-content bg-purple">
-              <img src="../../assets/logo.png" alt="">
+            <img src="../../assets/logo.png" alt>
           </div>
         </el-col>
         <el-col :span="19">
           <div class="grid-content bg-purple-light">
-              <h3>电商后台管理系统</h3>
+            <h3>电商管理后台</h3>
           </div>
         </el-col>
         <el-col :span="1">
           <div class="grid-content bg-purple">
-              <a href="#" class="loginout">退出</a>
+            <a href="#" class="loginout" @click.prevent="handleloginout()">退出</a>
           </div>
         </el-col>
       </el-row>
-      </el-header>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <!-- 是否只保持一个子菜单的展开 :unique-opened="true" -->
+        <!-- :router="true" 开启路由模式 -->
+        <el-menu
+          :router="true"
+          :unique-opened="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1用户列表">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>用户列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>角色列表</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>权限列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>商品列表</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>分类参数</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>商品分类</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>订单列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+              <template slot="title">
+                <i class="el-icon-sort"></i>
+                <span>数据列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+        
+        </el-menu>
+      </el-aside>
+
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+    // 在组件渲染之前 if token
+   // 钩子函数 在创建vm实例之前
+   beforeCreate() {
+       // 获取token
+       const token = localStorage.getItem('token')
+       // 如果不存在 回到login
+       if (!token) {
+           this.$router.push({name: 'login'})
+       }
+   },
+  methods: {
+    handleloginout() {
+      // 1. 提示
+      this.$message.success("退出成功");
+      // 2清除token
+      localStorage.clear()
+      // 3.回到 login组件
+      this.$router.push({name: 'login'})
+    }
+  }
+};
 </script>
 
 <style>
 .container {
   height: 100%;
 }
-.aside {
+.header {
   background-color: #b3c0d1;
 }
-.header {
+.aside {
   background-color: #d3dce6;
 }
 .main {
   background-color: #e9eef3;
   height: 100%;
 }
-/* 头部样式调整 */
 .bg-purple-light {
-    text-align: center;
+  text-align: center;
 }
 .loginout {
-    text-decoration: none;
-    line-height: 60px;
+  text-decoration: none;
+  line-height: 60px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ export default new Router({
   routes: [
     {
       name: 'login',
-      path: '/login',
+      path: '/',
       component: Login
     },
     {
